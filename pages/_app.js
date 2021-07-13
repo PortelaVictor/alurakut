00e086f16e14da5b8f64825298ddf952341201ec
Victor Portela <portelavictor@hotmail.com>
2021-07-13 11:59:09 -0300

    Inicio do alurakut

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,50 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
+const GlobalStyle = createGlobalStyle`  
+  /*Reset CSS*/
+  {
+    margin:0;
     padding: 0;
     box-sizing: border-box;
   }
+
+  body {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: sans-serif;
+      background-color: #D9E6F6;
+    }
+    
+  #__next {
+    display: flex;
+    main-height: 100 vh;
+    flex-direction: column;
+  }
+
+  img{
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+  const theme = {
+    colors: {
+      primary: 'red',
+    },
+  }
 
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  )
+  export default function App({ Component, pageProps }) {
+    return (
+      <>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </>
+    )
 }
